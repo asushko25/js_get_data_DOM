9bc5fb1b0db2524829a2f4cf6d1fff95523e6e48
Oleksandr Sushko <alexandrsushko25@gmail.com>
2024-07-08 19:01:59 +0200

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,27 @@
+"use strict";
+const elements = [
+    ...document.querySelectorAll(".population")
+];
+const numbers = [];
+elements.forEach((element)=>{
+    const numStr = element.textContent.trim().split(",").join("");
+    const num = Number(numStr); // Преобразуем строку в число
+    numbers.push(num);
+});
+function sumNumbers() {
+    let sum = 0;
+    for(let i = 0; i < numbers.length; i++)sum += numbers[i];
+    return sum.toLocaleString("en-US");
+}
+function averageNumbers() {
+    let average = 0;
+    for(let i = 0; i < numbers.length; i++)average += numbers[i];
+    const avarageNum = average / numbers.length;
+    return avarageNum.toLocaleString("en-US");
+}
+const textSumContent = document.querySelector(".total-population"); // sum all
+textSumContent.innerHTML = `${sumNumbers()}`;
+const textAverageContent = document.querySelector(".average-population"); // avg
+textAverageContent.innerHTML = `${averageNumbers()}`;
+
+//# sourceMappingURL=index.f75de5e1.js.map
